0ffb7896bb257123823717043f25cf69353496df
MK2k <gitlab@mk2k.net>
2020-12-06 13:54:23 +0100

    build.sh: use RELEASE/bigbox3d directory

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 rm -rf ./RELEASE/
-mkdir -p ./RELEASE
+mkdir -p ./RELEASE/bigbox3d
 
-npx uglify-js ./bigbox3d.js --output ./RELEASE/bigbox3d.js
-cp ./bigbox3d.html ./RELEASE/bigbox3d.html
-cp ./template* ./RELEASE/
-cp ./README.md ./RELEASE/README.md
-cp ./LICENSE.md ./RELEASE/LICENSE.md
+npx uglify-js ./bigbox3d.js --output ./RELEASE/bigbox3d/bigbox3d.js
+cp ./bigbox3d.html ./RELEASE/bigbox3d/bigbox3d.html
+cp ./template* ./RELEASE/bigbox3d/
+cp ./README.md ./RELEASE/bigbox3d/README.md
+cp ./LICENSE.md ./RELEASE/bigbox3d/LICENSE.md
