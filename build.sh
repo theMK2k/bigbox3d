84ba011b18c204233a62bf6083c460c595172a61
MK2k <gitlab@mk2k.net>
2022-04-03 17:36:37 +0200

    build.sh fixed

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 rm -rf ./RELEASE/
 mkdir -p ./RELEASE/bigbox3d
 
-npx babel bigbox3d.js --presets es2015 -o bigbox3d-babel.js
+npx babel@5 bigbox3d.js --presets es2015 -o bigbox3d-babel.js
 npx uglify-js ./bigbox3d-babel.js --output ./RELEASE/bigbox3d/bigbox3d.js
 rm bigbox3d-babel.js
 cp ./bigbox3d.html ./RELEASE/bigbox3d/bigbox3d.html
